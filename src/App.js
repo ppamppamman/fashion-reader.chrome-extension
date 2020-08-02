dcf7562cadd6e7004c964a2aae188c52e995df6e
Jaeuk-YOO <ryou9305@naver.com>
2020-08-03 05:45:39 +0900

    manifest 세팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import styled, {createGlobalStyle} from 'styled-components';
+
 import Speech from './components/Speech';
 import Hotkey from './components/Hotkey';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    min-width: 100%;
+    height: 600px;
+  }
+`
+
+const AppStyleBlock = styled.div`
+  background-color: #dddddd;
+  width:400px;
+`
 
 function App() {
   return (
     <>
-      <Speech />
-      <Hotkey />
+      <GlobalStyle />
+      <AppStyleBlock>
+        <Speech />
+        <Hotkey />
+      </AppStyleBlock>
     </>
   )
 }
